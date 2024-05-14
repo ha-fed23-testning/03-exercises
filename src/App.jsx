4342f1606ee0a04e0e0157a173e74fcd212eba61
David Andersson <lejonmanen@gmail.com>
2024-05-15 00:09:27 +0200

    Template with basic styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,33 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+	const [count, setCount] = useState(0)
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+	return (
+		<>
+		<header>
+			<nav>
+				<div className="selected"> Logga in </div>
+				<div> Miniräknare </div>
+				<div> Todo-lista </div>
+				<div> Kundvagn </div>
+			</nav>
+		</header>
+		<main>
+			<h2> Välkommen gäst </h2>
+			<section className="form">
+				<label> Namn </label>
+				<input type="text" value="Kalle" />
+				<button> Spara namn </button>
+			</section>
+
+
+
+
+		</main>
+		</>
+	)
 }
 
 export default App
